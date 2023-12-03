3eeb85ebd6cd9444a078bd42a343e886d6012115
aid6n_ <padosory5@gmail.com>
2023-12-02 20:50:58 -0800

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,40 +1,60 @@
-
 import './App.css';
 import React from 'react';
 import AirportDropdown from './ChooseAirport';
-import FlightInput from './InputFlight'
+import FlightInput from './InputFlight';
 import LocationComponent from './StartLocation';
 import DriveCheckBox from './DriveCheckBox';
 import TsaCheckBox from './DriveOption';
 
-
 const TitleComponent = () => {
   return (
     <div>
+      {/* Header */}
       <header className="title-header">
         <h1>AirExpress</h1>
       </header>
+
+      {/* Location Component */}
       <div className="flight-selector">
         <LocationComponent />
       </div>
+
+      {/* Info Text */}
       <div className="info-text">
         <p>Place in your info</p>
       </div>
+
+      {/* Airport Dropdown */}
       <div className="input-box">
         <div className="dropdown-container" style={{ display: 'flex', alignItems: 'center' }}>
-          <p style={{ fontSize: '30px', marginRight: '20px' }}>Place in your info</p>
-          <div style={{ marginBottom: '30px' }}>
+          <p style={{ fontSize: '30px', fontWeight: 'bold', marginLeft: '200px' }}>Place in your info</p>
+          <div style={{ marginBottom: '30px'}}>
             <AirportDropdown />
           </div>
         </div>
-        <div className="flight-selector">
-          <FlightInput />
+
+        {/* Flight Input */}
+        <div className="dropdown-container" style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ fontSize: '30px', fontWeight: 'bold', marginLeft: '160px' }}>Input Flight Number</p>
+          <div className="flight-selector" style={{ marginBottom: '8px'}} >
+            <FlightInput />
+          </div>
         </div>
-        <div className="flight-selector">
-        <DriveCheckBox />
+
+        {/* Drive Checkbox */}
+        <div className="dropdown-container" style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ fontSize: '30px', fontWeight: 'bold', marginLeft: '300px' }}></p>
+          <div className="flight-selector">
+            <DriveCheckBox />
+          </div>
         </div>
-        <div className="flight-selector">
-          <TsaCheckBox />
+
+        {/* TSA Checkbox */}
+        <div className="dropdown-container" style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ fontSize: '30px', fontWeight: 'bold', marginLeft: '320px' }}></p>
+          <div className="flight-selector">
+            <TsaCheckBox />
+          </div>
         </div>
       </div>
     </div>
